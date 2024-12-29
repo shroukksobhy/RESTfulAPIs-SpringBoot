629d85b18374cd319a05196fe8485095da18424c
shroukksobhy <shroukksobhy@gmail.com>
2024-12-29 04:18:32 +0200

    added helloWorldInternationalized

--- src/main/java/com/exampleSpringBoot/restful_web_services/HelloWorldController.java
+++ src/main/java/com/exampleSpringBoot/restful_web_services/HelloWorldController.java
@@ -1,11 +1,23 @@
 package com.exampleSpringBoot.restful_web_services;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	private MessageSource messageSource;
+	
+
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+	
 	
 	@GetMapping("/hello-world")
 	public String helloWorld1() {
@@ -20,6 +32,14 @@ public class HelloWorldController {
 	@GetMapping("/hello-world/path-v/{name}")
 	public HelloBeans helloWorldPathV(@PathVariable String name) {
 		return new HelloBeans("Hello "+ name);
+	}
+	
+	@GetMapping("/hello-world-languages")	
+	public String helloWorldInternationalized() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("gm.message", null, "Defauld Message", locale);
+		
+		
 	}
 
 }
